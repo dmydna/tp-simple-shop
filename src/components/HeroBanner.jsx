519404f2972a96b4be3964fbfbe6265c73106a6d
dmydna <danimaydana9@gmail.com>
2025-10-09 11:43:41 -0300

    actualiza Hero

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,20 +1,24 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 
 function HeroBanner({image, children, variant}){
 
+   const bgColor = `bg-${!variant ? 'white' : variant}`
+   const textColor = `text-${variant && variant === 'white' || variant === 'light' ? 'dark' : 'light'}`
+
     return(
-        <div className={`py-3 bg-${!variant ? 'white' : variant}`}>
-        <Container className="d-flex">
-          <div className="p-5">
-            <img width={130} src={image} alt="" />
+        <Container className={`py-3 ${bgColor}`}>
+        <Row className={`${bgColor}`}>
+          <div className="p-md-5 p-2 col-lg-3 col-md-4 col-sm-6 col-12 order-md-2">
+            <img  className="d-block mx-auto" width={130} src={image} alt="" />
           </div>
-          <div className={`py-5 text-${variant && variant === 'white' 
-                                     || variant === 'light' ? 'dark' : 'light'}`}>
+          <div className={`col-lg-9 col-md-4 col-sm-6 col-12 order-md-1
+          p-5 ${textColor}`}>
                 {children}
           </div>
-        </Container>
-      </div> 
+
+        </Row>
+      </Container> 
     )
 }
 
